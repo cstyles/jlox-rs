6318e7b932bcd075a25e9e5cd003d0708d86375b
Collin Styles <collingstyles@gmail.com>
2021-11-15 19:27:22 -0800

    Chapter 5 - Representing Code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,56 @@
-use std::io::{BufRead, Write};
+use std::{
+    fmt::Display,
+    io::{BufRead, Write},
+};
 
 mod scanner;
 mod token;
 
 use scanner::Scanner;
+use token::{Token, TokenType};
+
+enum Expr {
+    Binary(
+        Box<Expr>, // left
+        Token,     // operator
+        Box<Expr>, // right
+    ),
+    Grouping(Box<Expr>),
+    Literal(token::Literal),
+    Unary(
+        Token,     // operator
+        Box<Expr>, // right
+    ),
+}
+
+impl Display for Expr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expr::Binary(left, operator, right) => {
+                write!(f, "({} {} {})", operator.lexeme, left, right)
+            }
+            Expr::Grouping(expr) => write!(f, "(group {})", expr),
+            Expr::Literal(literal) => write!(f, "{}", literal),
+            Expr::Unary(operator, right) => write!(f, "({} {})", operator.lexeme, right),
+        }
+    }
+}
+
+impl Expr {
+    fn binary(left: Self, operator: Token, right: Self) -> Self {
+        Self::Binary(Box::new(left), operator, Box::new(right))
+    }
+
+    fn grouping(expr: Self) -> Self {
+        Self::Grouping(Box::new(expr))
+    }
+
+    fn unary(operator: Token, right: Self) -> Self {
+        Self::Unary(operator, Box::new(right))
+    }
+}
+
+trait Visitor<R> {}
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
@@ -55,4 +102,17 @@ fn run_file(filename: &str) {
     if run(program).is_err() {
         std::process::exit(65);
     }
+}
+
+fn _test_pretty_print() {
+    let expr = Expr::binary(
+        Expr::unary(
+            Token::new(TokenType::Minus, "-".into(), token::Literal::None, 1),
+            Expr::Literal(token::Literal::Number(123.0)),
+        ),
+        Token::new(TokenType::Star, "*".into(), token::Literal::None, 1),
+        Expr::grouping(Expr::Literal(token::Literal::Number(45.67))),
+    );
+
+    println!("{}", expr);
 }
--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone)]
 pub struct Token {
     pub(crate) token_type: TokenType,
@@ -6,13 +8,34 @@ pub struct Token {
     pub(crate) line: usize,
 }
 
-impl std::fmt::Display for Token {
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: Literal, line: usize) -> Self {
+        Self {
+            token_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+}
+
+impl Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "{:?} {} {:?}",
             self.token_type, self.lexeme, self.literal
         )
+    }
+}
+
+impl Display for Literal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Literal::None => write!(f, "nil"),
+            Literal::String(string) => write!(f, "{}", string),
+            Literal::Number(number) => write!(f, "{}", number),
+        }
     }
 }
 
